44caac3026d8a13479846e78c3eed16f432025a7
penguinwokrs <dev.and.penguin@gmail.com>
2016-12-31 23:06:48 +0900

    top page追加

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,11 +4,13 @@ lock '3.2.1'
 set :application, 'omote'
 set :repo_url, 'git@github.com:penguinwokrs/omote.git'
 
+set :rbenv_path, '/usr/local/rbenv'
+
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
 
 # Default deploy_to directory is /var/www/my_app
-# set :deploy_to, '/var/www/my_app'
+set :deploy_to, '/home/apps/omote'
 
 # Default value for :scm is :git
 # set :scm, :git
@@ -23,16 +25,16 @@ set :repo_url, 'git@github.com:penguinwokrs/omote.git'
 # set :pty, true
 
 # Default value for :linked_files is []
-# set :linked_files, %w{config/database.yml}
+set :linked_files, %w{config/database.yml}
 
 # Default value for linked_dirs is []
-# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
 # Default value for keep_releases is 5
-# set :keep_releases, 5
+set :keep_releases, 5
 
 namespace :deploy do
 
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,7 +4,7 @@
 # is considered to be the first unless any hosts have the primary
 # property set.  Don't declare `role :all`, it's a meta role.
 
-role :app, %w{deploy@example.com}
+role :app, %w{apps@ec2-54-199-246-203.ap-northeast-1.compute.amazonaws.com}
 # role :web, %w{deploy@example.com}
 # role :db,  %w{deploy@example.com}
 
@@ -15,7 +15,7 @@ role :app, %w{deploy@example.com}
 # server list. The second argument is a, or duck-types, Hash and is
 # used to set extended properties on the server.
 
-server 'example.com', user: 'apps', roles: %w{web app}, my_property: :my_value
+server 'ec2-54-199-246-203.ap-northeast-1.compute.amazonaws.com', user: 'apps', roles: %w{app}
 
 
 # Custom SSH Options
@@ -25,11 +25,11 @@ server 'example.com', user: 'apps', roles: %w{web app}, my_property: :my_value
 #
 # Global options
 # --------------
-#  set :ssh_options, {
-#    keys: %w(/home/rlisowski/.ssh/id_rsa),
-#    forward_agent: false,
-#    auth_methods: %w(password)
-#  }
+
+ set :ssh_options, {
+   keys:"#{ENV.fetch('HOME')}/ssh/private/omote-production.pem)",
+   forward_agent: false
+ }
 #
 # And/or per server (overrides global)
 # ------------------------------------
