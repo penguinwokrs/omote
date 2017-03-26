db0384a3e1b1dd58f1e48d008c757c83de207291
kazuyuki ikeda <gankai1104@gmail.com>
2017-03-27 01:00:35 +0900

    remove message_id header and bug fix

--- app/mailers/authentication_mailer.rb
+++ app/mailers/authentication_mailer.rb
@@ -2,8 +2,6 @@
 class AuthenticationMailer < ApplicationMailer
   default from: ENV['FROM_MAIL_ADDRESS']
 
-  after_action :set_message_id
-
   def notification(authentication)
     @authentication = authentication
     mail(
@@ -20,9 +18,5 @@ class AuthenticationMailer < ApplicationMailer
       to: ENV['TO_MAIL_ADDRESS'],
       subject: "TRUST DOCKから認証結果が届きました(#{@authentication.name})"
     )
-  end
-
-  def set_message_id
-    headers['Message-ID'] = "#{@authentication.message_id}@#{ENV['DOMAIN']}"
   end
 end
--- db/migrate/20170326023524_add_message_id_to_authentication.rb
+++
@@ -1,5 +0,0 @@
-class AddMessageIdToAuthentication < ActiveRecord::Migration[5.0]
-  def change
-    add_column :authentications, :message_id, :string, default: ''
-  end
-end
